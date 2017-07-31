b772abb55083a738b91d0c4f30f7d7c93a0ca1f0
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2017-07-31 17:40:14 +0900

    install_hook.sh: support array of targets
    make it possible to install/uninstall hooks for multiple applications.
    
    example:
    
          install_hook.sh install /usr/bin/app1 /usr/bin/app2 /usr/bin/app3
          install_hook.sh uninstall /usr/bin/app1 /usr/bin/app2 /usr/bin/app3

--- install_hook.sh
+++ install_hook.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#
+# Copyright (C) 2017 Sergey Senozhatsky
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+
 MTRACE_PATH=./build/lib/
 
 function remount_rootfs_rw
@@ -105,7 +124,7 @@ function uninstall
 	return 0
 }
 
-function main
+function process
 {
 	if [ "z$1" == "zinstall" -o "z$1" == "zrinstall" ]; then
 		local remount=0
@@ -115,7 +134,7 @@ function main
 		fi
 
 		install $2 $remount
-		exit $?
+		return $?
 	fi
 
 	if [ "z$1" == "zuninstall" -o "z$1" == "zruninstall" ]; then
@@ -124,14 +143,36 @@ function main
 		if [ "z$1" == "zruninstall" ]; then
 			remount=1
 		fi
+
 		uninstall $2 $remount
-		exit $?
+		return $?
 	fi
+
+	return 7
 }
 
-main $1 $2
+function print_usage
+{
+	echo "Usage example:"
+	echo "install_hook.sh install|uninstall /path/application"
+	echo "                rinstall|runinstall - to force remount of FS in rw mode"
+}
 
-echo "Usage example:"
-echo "install_hook.sh install|uninstall /path/application"
-echo "                rinstall|runinstall - to force remount of FS in rw mode"
-exit 3
+hook_args=("$@")
+hook_args_num=${#hook_args[@]}
+
+if [ $hook_args_num -le 1 ]; then
+	print_usage
+	exit 3
+fi
+
+for (( i=1; i<${hook_args_num}; i++ )); do
+	process ${hook_args[0]} ${hook_args[$i]}
+	code=$?
+	if [ $code != 0 ]; then
+		print_usage
+		exit $code
+	fi
+done
+
+exit 0
